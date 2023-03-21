289f259a84601a0ae7fcd1d5d59e4e42281094a4
David Domínguez Blanco <david.dominguez.blanco.2001@gmail.com>
2023-03-21 10:21:48 +0100

    Add project files.

---
+++ MvcSeguridadDoctores/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace MvcSeguridadDoctores.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
